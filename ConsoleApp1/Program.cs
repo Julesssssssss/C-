a14c2b04fe84441c352527be9298de708dc55556
JBELIEN <jules.belien@viacesi.fr>
2023-03-06 15:46:46 +0100

    features(introduction) fonction de calcul salaires, par mois sur l'année complete

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Win32.SafeHandles;
+using System.Collections;
+
 class Program
 
 {
@@ -15,7 +18,7 @@ class Program
 
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Entrez dabord votres salaire, puis les taxes:");
-        Console.WriteLine("Votre salaire a l'année: ");
+        Console.WriteLine("Votre salaire a l'année (en brut): ");
         bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
         if (salaryIsInt)
         {
@@ -25,7 +28,7 @@ class Program
             if (taxIsInt)
             {
                 Show(salary, taxes);
-
+                
                 if (salary > 50000)
                 {
                     Console.WriteLine("Il faudrait faire des dons pour réduire tes impots");
@@ -38,6 +41,7 @@ class Program
                 {
                     Console.WriteLine("Je te conseille de venir faire un bac +5 en dev au CESI");
                 }
+                salary = ((salary / 12) - ((salary / 12) * taxes / 100));
             }
             else
             {
@@ -50,12 +54,47 @@ class Program
         }
 
 
+        string[] mois = new string[12];
+        mois[0]= "Janvier";
+        mois[1]= "Fevrier";
+        mois[2]= "Mars";
+        mois[3]= "Avril";
+        mois[4]= "Mai";
+        mois[5]= "Juin";
+        mois[6]= "Juillet";
+        mois[7]= "Aout";
+        mois[8]= "Septembre";
+        mois[9]= "Octobre";
+        mois[10]= "Novembre";
+        mois[11]= "Decembre";
+
+        foreach (string mo in mois)
+        {
+            if (mo == "Decembre")
+            {
+                Console.WriteLine("En " + mo + " votre salaire est de: " + (salary + salary * 10/100) + "€");
+            }
+            else if(mo == "Aout")
+            {
+                Console.WriteLine("En " + mo + " vous n'avez pas de salaire puisque l'entreprise est fermé");
+            }
+            else
+            {
+                Console.WriteLine("En " + mo + " votre salaire est de: " + salary + "€");
+            }
+        }
+
+
+
+
     }
     static void Show(float a, float b)
     {
-        Console.WriteLine("Salaire: " + a / 12 + "€");
+        Console.WriteLine("Salaire brut mensuel: " + a / 12 + "€");
         Console.WriteLine("Taux d'imposition: " + b + "%");
         Console.WriteLine("Votre salaire net, apres l'imposition est de: " + ((a / 12) - ((a / 12)) * b / 100) + "€");
 
     }
 }
+
+
